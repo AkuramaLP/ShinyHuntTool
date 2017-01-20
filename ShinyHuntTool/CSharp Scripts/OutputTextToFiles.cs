d93df86a3798a4379b5ce30c308cbaea7f4c1869
AkuramaLP <jonathan.witt1998@gmail.com>
2017-01-20 14:15:12 +0100

    First Commit
    -Added all Project Files

---
+++ ShinyHuntTool/CSharp Scripts/OutputTextToFiles.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShinyHuntTool
+{
+    class OutputTextToFiles
+    {
+        Counter count = new Counter();
+
+        public void startActionCounter()
+        {
+            string textOutputToFile = count.countChain.ToString();
+            System.IO.File.WriteAllText(@"C:\Users\Jonathan.Witt\Desktop\TEST\ShinyCounter.txt", textOutputToFile);
+        }
+
+        public void writeToFileCounter()
+        {
+            string textOutputToFile = count.countChain.ToString();
+            System.IO.File.WriteAllText(@"C:\Users\Jonathan.Witt\Desktop\TEST\Shiny.txt", textOutputToFile);
+        }
+
+        public void saveCounter()
+        {
+            string textOutputToFile = count.countChain.ToString();
+            System.IO.File.WriteAllText(@"C:\Users\Jonathan.Witt\Desktop\TEST\Shiny.txt", textOutputToFile);
+        }
+    }
+}
---
+++ ShinyHuntTool/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShinyHuntTool
+{
+    public partial class Form1 : Form
+    {
+        OutputTextToFiles OTTF = new OutputTextToFiles();
+
+        public Form1()
+        {
+            InitializeComponent();
+            OTTF.startActionCounter();
+        }
+
+        //private void inputCheck_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        //{
+        //    if(e.KeyCode == Keys.Add)
+        //    {
+
+        //    }
+        //}
+    }
+}
